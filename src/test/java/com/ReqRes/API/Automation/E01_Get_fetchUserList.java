8341afe409d444ae02630a3a9d5ecc901b56ff26
MoemenMohamed98 <moemenmohamed20@gmail.com>
2024-08-07 03:15:27 +0300

    Update_Validations

--- src/test/java/com/ReqRes/API/Automation/E01_Get_fetchUserList.java
+++ src/test/java/com/ReqRes/API/Automation/E01_Get_fetchUserList.java
@@ -2,28 +2,46 @@ package com.ReqRes.API.Automation;
 
 import Utilities.ObjectModels;
 import com.shaft.driver.SHAFT;
+import com.shaft.validation.Validations;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 public class E01_Get_fetchUserList {
    private SHAFT.API api;
 
 
-    @Test
+    @Test(priority = 1)
     public void valid_getTheUserList() {
+
+        // Initiate the Base URL
         api= new SHAFT.API(ObjectModels.BaseURL);
+
+        // Set the type of the method / endpoint / Status Code
         api
                 .get("/users")
                 .setTargetStatusCode(200)
                 .perform();
 
+        // Print response body
         System.out.println(api.getResponseBody());
 
+
+                 //******************************** Assertions ********************************//
+
+        // Assert that Response time is Less than or Equals 2000 MS
         api
-                .assertThatResponse().time().isLessThanOrEquals(3000).perform();
+                .assertThatResponse().time().isLessThanOrEquals(2000).perform();
 
         // Assert that Response body is not null
+        api
+                .assertThatResponse()
+                .body().isNotNull()
+                .withCustomReportMessage("The Response body for get profile request is Not Null").perform();
+
+
         api.assertThatResponse()
                 .body().contains("data")
                 .withCustomReportMessage("The Response body for fetching the user list is Not Null").perform();
@@ -32,46 +50,66 @@ public class E01_Get_fetchUserList {
                 .extractedJsonValue("page")
                 .isEqualTo("1").perform();
 
-//        api
-//                .assertThatResponse()
-//                .body().isNotNull()
-//                .withCustomReportMessage("The Response body for get profile request is Not Null").perform();
+
     }
 
-        @Test
+        @Test(priority = 2)
         public void valid_fetchTheUserList() {
-            api= new SHAFT.API(ObjectModels.BaseURL);
-//            given()
-//                    .baseUri(ObjectModels.BaseURL)
-//                    .log().all()
-//
-//            .when()
-//                    .get("users?page=2")
-//
-//            .then()
-//                    .log().all()
-//                    .assertThat().statusCode(200);
 
-            api
-                    .get("/users?page=2")
-                    .setTargetStatusCode(200)
-                    .perform();
+         Response response =
+                  given()
+                    .baseUri(ObjectModels.BaseURL)
+                    .log().all()
 
-            // Print response body
-            System.out.println(api.getResponseBody());
+                .when()
+                    .get("users?page=2")
 
-            api
-                    .assertThatResponse().time().isLessThanOrEquals(3000).perform();
+                 .then()
+                    .assertThat().statusCode(200)
+                    .extract().response();
 
-            // Assert that Response body is not null
-            api.assertThatResponse()
+
+            Validations
+                    .assertThat().response(response)
+                    .time().isLessThanOrEquals(2000).perform();
+
+            Validations
+                    .assertThat().response(response)
                     .body().contains("data")
                     .withCustomReportMessage("The Response body for fetching the user list is Not Null").perform();
 
-            api.assertThatResponse()
+//            Validations
+//                    .assertThat().response(response)
+//                    .body().isNotNull()
+//                    .withCustomReportMessage("The Response body for get profile request is Not Null").perform();
+
+            Validations
+                    .assertThat().response(response)
                     .extractedJsonValue("page")
                     .isEqualTo("2").perform();
 
+
+
+//            api= new SHAFT.API(ObjectModels.BaseURL);
+//            api
+//                    .get("/users?page=2")
+//                    .setTargetStatusCode(200)
+//                    .perform();
+//
+//            // Print response body
+//            System.out.println(api.getResponseBody());
+//
+//            api
+//                    .assertThatResponse().time().isLessThanOrEquals(3000).perform();
+//
+//            // Assert that Response body is not null
+//            api.assertThatResponse()
+//                    .body().contains("data")
+//                    .withCustomReportMessage("The Response body for fetching the user list is Not Null").perform();
+//
+//            api.assertThatResponse()
+//                    .extractedJsonValue("page")
+//                    .isEqualTo("2").perform();
 
         }
 
--- src/test/java/com/ReqRes/API/Automation/E05_Delete_deleteUser.java
+++ src/test/java/com/ReqRes/API/Automation/E05_Delete_deleteUser.java
@@ -1,4 +1,40 @@
 package com.ReqRes.API.Automation;
 
-public class E06_Delete_deleteUser {
+import Utilities.ObjectModels;
+import com.shaft.driver.SHAFT;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class E05_Delete_deleteUser {
+
+    private SHAFT.API api;
+
+    @Test
+    public void valid_Delete_User_byID() {
+
+        // Initiate the Base URL
+        api = new SHAFT.API(ObjectModels.BaseURL);
+
+        // Set the type of the method / endpoint / Status Code
+        api
+                .delete("/users/2")
+                .setTargetStatusCode(204)
+                .perform();
+
+              //******************************** Assertions ********************************//
+
+        // Assert that Response time is Less than or Equals 2000 MS
+        api
+                .assertThatResponse().time().isLessThanOrEquals(2000).perform();
+
+        // Assert that the Response body is null and the user is deleted successfully
+        api
+                .assertThatResponse()
+                .body().isNotNull()
+                .withCustomReportMessage("The user info is deleted successfully")
+                .perform();
+
+    }
 }
